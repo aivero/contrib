196e97e999950f9248ec525b7677abc5017f30af
Jimmi Holst Christensen <jhc@dismail.de>
2022-06-04 17:32:10 +0200

    Fix xorgproto requirement

--- recipes/libglvnd/conanfile.py
+++ recipes/libglvnd/conanfile.py
@@ -16,12 +16,18 @@ class Libglvnd(Recipe):
     def requirements(self):
         if self.options.x11:
             self.requires("libxext/[^1.3.4]", "private")
-            self.requires("xorgproto/[^2020.1]", "private")
+        self.requires("xorgproto/[>=2020.1]", "private")
 
     def source(self):
-        self.get(f"https://gitlab.freedesktop.org/glvnd/libglvnd/-/archive/v{self.version}/libglvnd-v{self.version}.tar.gz")
+        self.get(
+            f"https://gitlab.freedesktop.org/glvnd/libglvnd/-/archive/v{self.version}/libglvnd-v{self.version}.tar.gz"
+        )
         # Hardcode glvnd vendor path, since conan prefixes the package name in env vars starting with __, so __DEFAULT_EGL_VENDOR_CONFIG_DIRS cannot be set
-        tools.replace_in_file(os.path.join(self.src, "src", "EGL", "libeglvendor.c"), "DEFAULT_EGL_VENDOR_CONFIG_DIRS", '"/usr/share/glvnd/egl_vendor.d"')
+        tools.replace_in_file(
+            os.path.join(self.src, "src", "EGL", "libeglvendor.c"),
+            "DEFAULT_EGL_VENDOR_CONFIG_DIRS",
+            '"/usr/share/glvnd/egl_vendor.d"',
+        )
 
     def build(self):
         opts = {

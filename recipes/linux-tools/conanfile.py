c6cfd1336f241a5f4d01f42838500f5498099f43
Jimmi Holst Christensen <jhc@dismail.de>
2022-06-05 00:50:16 +0200

    Revert "linux-tools: build with bootstrap"
    This reverts commit dbf285d33adf1ef7fe7aae1b8c5ca65857e423bd.

--- recipes/linux-tools/conanfile.py
+++ recipes/linux-tools/conanfile.py
@@ -4,12 +4,13 @@ from build import *
 class LinuxTools(Recipe):
     description = "Linux tools"
     license = "GPL"
+    build_requires = ("cc/[^1.0.0]", "autotools/[^1.0.0]")
 
     def source(self):
         self.get(f"https://cdn.kernel.org/pub/linux/kernel/v{self.version.split('.')[0]}.x/linux-{self.version}.tar.xz")
 
     def build(self):
         args = [
-            "WERROR=0",
+            "WERROR=0"
         ]
         self.make(args, os.path.join(self.src, "tools", "perf"))

b6bd69183e73797753cfd699ca95e0ef2727f2b6
Jimmi Holst Christensen <jhc@dismail.de>
2022-06-20 16:42:39 +0200

    cc: Remove lto and -fPIC
    This was the only way to get rustc to compile.

--- recipes/cc/conanfile.py
+++ recipes/cc/conanfile.py
@@ -10,15 +10,23 @@ class CC(Recipe):
         self.requires(f"llvm/[^{self.settings.compiler.version}]")
 
     def package_info(self):
+        llvm_deps_cpp_info = self.deps_cpp_info["llvm"]
+        llvm_rootpath = llvm_deps_cpp_info.rootpath
         static_flags = ""
         if self.settings.libc == "musl":
             static_flags = "-static"
         libc_inc = self.env["LIBC_INCLUDE_PATH"]
-        libclang_inc = os.path.join(self.deps_cpp_info["llvm"].rootpath, "lib", "clang", self.deps_cpp_info["llvm"].version, "include")
-        llvm_inc = os.path.join(self.deps_cpp_info["llvm"].rootpath, "include")
-        libcxx_inc = os.path.join(self.deps_cpp_info["llvm"].rootpath, "include", "c++", "v1")
+        libclang_inc = os.path.join(
+            llvm_rootpath,
+            "lib",
+            "clang",
+            llvm_deps_cpp_info.version,
+            "include",
+        )
+        llvm_inc = os.path.join(llvm_rootpath, "include")
+        libcxx_inc = os.path.join(llvm_rootpath, "include", "c++", "v1")
         # -Wno-unused-command-line-argument is needed for some sanity tests in cmake
-        cflags = f" -nostdinc -idirafter {libclang_inc} -idirafter {libc_inc} -idirafter {llvm_inc} {static_flags} -fPIC -flto=thin -Wno-unused-command-line-argument "
+        cflags = f" -nostdinc -idirafter {libclang_inc} -idirafter {libc_inc} -idirafter {llvm_inc} {static_flags} -Wno-unused-command-line-argument "
         cxxflags = f" -nostdinc++ -idirafter {libcxx_inc} {cflags} "
 
         self.env_info.CFLAGS = cflags

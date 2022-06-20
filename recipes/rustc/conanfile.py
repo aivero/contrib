591a77df2f3f5601ee60e40973d479b7aac48eac
Jimmi Holst Christensen <jhc@dismail.de>
2022-06-20 09:23:52 +0200

    rustc: Download none forked version of compiler-rt with rust
    This copies the way arch linux builds the rust compiler.

--- recipes/rustc/conanfile.py
+++ recipes/rustc/conanfile.py
@@ -21,10 +21,23 @@ class Rustc(Recipe):
     )
 
     def source(self):
-        self.get(f"https://static.rust-lang.org/dist/rustc-{self.version}-src.tar.gz")
+        llvm_version = {"1.61.0": "14.0.5"}[self.version]
+        self.get(
+            f"https://static.rust-lang.org/dist/rustc-{self.version}-src.tar.gz",
+            dest_folder=os.path.join(self.src, "rust"),
+        )
+        self.get(
+            f"https://github.com/llvm/llvm-project/releases/download/llvmorg-{llvm_version}/compiler-rt-{llvm_version}.src.tar.xz",
+            dest_folder=os.path.join(self.src, "compiler-rt"),
+            src_folder=f"compiler-rt-{llvm_version}.src",
+        )
 
     def build(self):
+        rust_folder = os.path.join(self.build_folder, self.src, "rust")
+        compiler_rt_folder = os.path.join(self.build_folder, self.src, "compiler-rt")
         os.environ["RUSTFLAGS"] = "-g -Clinker-plugin-lto -Copt-level=2"
+        os.environ["RUST_COMPILER_RT_ROOT"] = compiler_rt_folder
+
         arch = {"x86_64": "x86_64", "armv8": "aarch64"}[str(self.settings.arch)]
         triple = f"{arch}-unknown-linux-gnu"
         args = [
@@ -32,6 +45,7 @@ class Rustc(Recipe):
             f"--target={triple}",
             f'--prefix="{self.package_folder}"',
             f'--sysconfdir="{self.package_folder}/etc"',
+            f"--llvm-root={self.deps_cpp_info['llvm'].rootpath}",
             "--release-channel=stable",
             "--set=llvm.thin-lto=true",
             "--tools=src,cargo,rustfmt,clippy,rust-analyzer",
@@ -39,16 +53,14 @@ class Rustc(Recipe):
             "--enable-locked-deps",
             "--enable-extended",
             "--enable-vendor",
-            "--enable-clang",
-            "--enable-lld",
-            "--enable-use-libcxx",
+            "--enable-llvm-link-shared",
             "--disable-docs",
             "--disable-compiler-docs",
             "--disable-llvm-static-stdcpp",
         ]
 
-        self.exe("./configure", args)
-        self.exe("python x.py install")
+        self.exe(os.path.join(rust_folder, "configure"), args)
+        self.exe("python", [os.path.join(rust_folder, "x.py"), "install"])
 
     def package_info(self):
         self.env_info.RUSTFLAGS = "-g -Copt-level=2"

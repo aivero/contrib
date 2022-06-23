d830f601627012115c570d16bf4de3f0d0835a99
Jimmi Holst Christensen <jhc@dismail.de>
2022-06-23 15:30:02 +0200

    intel-gmmlib: Update to version 22

--- recipes/intel-media-driver/conanfile.py
+++ recipes/intel-media-driver/conanfile.py
@@ -6,23 +6,22 @@ class IntelMediaDriver(Recipe):
     license = "MIT"
     build_requires = ("cc/[^1.0.0]", "cmake/[^3.18.4]")
     requires = (
-        "intel-gmmlib/[^21.3.5]",
+        "intel-gmmlib/[^22.1.3]",
         "libva/[^2.13.0]",
         "libpciaccess/[^0.16]",
     )
 
     def source(self):
-        self.get(
-            f"https://github.com/intel/media-driver/archive/intel-media-{self.version}.tar.gz"
-        )
+        self.get(f"https://github.com/intel/media-driver/archive/intel-media-{self.version}.tar.gz")
 
     def package(self):
         self.copy("iHD_drv_video.so", src="media_driver", dst="lib")
 
     def build(self):
         os.environ["CPATH"] += ":" + ":".join(
-            self.deps_cpp_info["libx11"].include_paths +
-            self.deps_cpp_info["xorgproto"].include_paths)
+            self.deps_cpp_info["libx11"].include_paths
+            + self.deps_cpp_info["xorgproto"].include_paths
+        )
         defs = {"BYPASS_MEDIA_ULT": "yes"}
         self.cmake(defs)
 

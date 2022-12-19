41eeb66e6b45fd2f50131e6bd7b0fc6bec6553fa
Niclas Overby <niclas@overby.me>
2022-12-19 10:45:50 +0100

    create_aliases: print more debug logs

--- scripts/create_aliases.py
+++ scripts/create_aliases.py
@@ -15,11 +15,11 @@ if "CI" in os.environ:
     print(f"Uploading internal to: {internal_repo}")
 
     fetch_dev_repo = os.environ["CONAN_REPO_DEV_ALL"]
-    print(f"Fetching from: {fetch_repo}")
+    print(f"Fetching from: {fetch_dev_repo}")
     public_dev_repo = os.environ["CONAN_REPO_DEV_PUBLIC"]
-    print(f"Uploading public to: {public_repo}")
+    print(f"Uploading public to: {public_dev_repo}")
     internal_dev_repo = os.environ["CONAN_REPO_DEV_INTERNAL"]
-    print(f"Uploading internal to: {internal_repo}")
+    print(f"Uploading internal to: {internal_dev_repo}")
     util.conan_init((fetch_repo, public_repo, internal_repo, fetch_dev_repo, public_dev_repo, internal_dev_repo))
 else:
     print("Not uploading any packages")

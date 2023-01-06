a4e49706f0277284c5f092ff9dbd44fddbed6e1c
Jimmi Holst Christensen <jhc@dismail.de>
2023-01-06 16:48:26 +0100

    Better require libpciaccess

--- recipes/libdrm/conanfile.py
+++ recipes/libdrm/conanfile.py
@@ -8,7 +8,7 @@ class Libdrm(Recipe):
         "cc/[^1.0.0]",
         "meson/[>=0.55.3]",
     )
-    requires = ("libpciaccess/[>=0.16]",)
+    requires = ("libpciaccess/[^0.16]",)
 
     def source(self):
         self.get(f"http://dri.freedesktop.org/libdrm/libdrm-{self.version}.tar.xz")

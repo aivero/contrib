eb2f9ef7d0d3ae983a937909c9f0ba4290d9347d
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2023-04-24 09:10:05 +0000

    Revert "rust: Do not set CARGO_TARGET_DIR"

--- recipes/rust/conanfile.py
+++ recipes/rust/conanfile.py
@@ -13,4 +13,5 @@ class RustRecipe(RustRecipe):
         # if not os.path.exists(cache_folder):
         #    os.makedirs(cache_folder)
         self.env_info.RUSTFLAGS = "-g"
+        self.env_info.CARGO_TARGET_DIR = os.path.join(self.conan_home, "cache", "cargo")
         self.env_info.CARGO_REGISTRIES_CRATES_IO_PROTOCOL = "sparse"

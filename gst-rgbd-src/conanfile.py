b1c1181d73a744df821c747898cbd05221d95fec
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2023-08-02 12:27:59 +0200

    rgbd-src: Don't depend on libk4a

--- gst-rgbd-src/conanfile.py
+++ gst-rgbd-src/conanfile.py
@@ -7,7 +7,7 @@ class GstRgbdSrc(GstRustProject):
     build_requires = ("rust/[^1.0.0]", "capnproto/[^0.8]")
     requires = (
         "capnproto/[^0.8.0]",
-        "libk4a/[^1.4.1]",
+        # "libk4a/[^1.4.1]",
         "librealsense/[^2.39.0]",
         "rust-libstd/[^1.0.0]",
         f"gst-depth-meta/{branch()}",
@@ -16,5 +16,5 @@ class GstRgbdSrc(GstRustProject):
     def build(self):
         # We need to rebuild the rust bindings every time we tag a new release.
         self.cargo(clean=[
-            "librealsense2", "librealsense2-sys", "libk4a", "libk4a-sys"
+            "librealsense2", "librealsense2-sys"
         ])
